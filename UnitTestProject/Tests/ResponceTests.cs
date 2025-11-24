e64bbf0c57efc9cd23662b9e233b4245721dea4c
Владислав Бевз <vladislav.bevz07@gmail.com>
2025-11-24 15:02:13 +0200

    Commit

--- UnitTestProject/Tests/ResponceTests.cs
+++ UnitTestProject/Tests/ResponceTests.cs
@@ -113,8 +113,6 @@ public class ResponceTests
             }
         };
 
-        result.Id = "id45";
-
         Assert.Equal(expected.Id, result.Id);
         Assert.Equal(expected.FirstName, result.FirstName);
         Assert.Equal(expected.LastName, result.LastName);
--- UnitTestProject/obj/Debug/net9.0/UnitTestProject.AssemblyInfo.cs
+++ UnitTestProject/obj/Debug/net9.0/UnitTestProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0226086f860ee08fcfb5851658e2edb9255aa6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1850bb29d8b2d0215700d54ba902271a38cc298")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitTestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitTestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
