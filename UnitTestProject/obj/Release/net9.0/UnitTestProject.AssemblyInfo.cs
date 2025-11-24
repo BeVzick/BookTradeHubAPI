d0226086f860ee08fcfb5851658e2edb9255aa6f
Владислав Бевз <vladislav.bevz07@gmail.com>
2025-11-24 05:08:51 +0200

    Commit

--- UnitTestProject/obj/Release/net9.0/UnitTestProject.AssemblyInfo.cs
+++ UnitTestProject/obj/Release/net9.0/UnitTestProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91b20e1561f4369162696ac92cfd414ff65ba710")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+864f98b97cab47d15b58e1e0cf990bdd18bfbed3")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitTestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitTestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
